d78a4f1f845036649167cb9a9c90ef62a5d65bc6
jaalonge1 <josh.alonge@digital-clarity.io>
2021-12-16 14:49:19 -0500

    Initial Commit

---
+++ Scaler/ScalerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScalerApp.swift
+//  Scaler
+//
+//  Created by Josh Alonge on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ScalerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
